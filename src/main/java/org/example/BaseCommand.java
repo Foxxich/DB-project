c5c5d74da7468ef69749a8dd6b634a882d9ae71d
Foxxich <64288356+Foxxich@users.noreply.github.com>
2021-01-24 11:34:35 +0100

    Improved backup

--- src/main/java/org/example/BaseCommand.java
+++ src/main/java/org/example/BaseCommand.java
@@ -14,32 +14,12 @@ public class BaseCommand {
     public void doBackUp() {
         {
             ArrayList<String> words = new ArrayList<>();
-            String[] command = new String[] {"cmd.exe", "/c", "\"C:\\Program Files\\MariaDB 10.5\\bin\\mysqldump.exe\" --quick --lock-tables --user=\"root\" --password=\"2020\" zakupy > \"C:\\Users\\Vadym\\Documents\\testB\\file.sql\" "};
+            String[] command = new String[]{"cmd.exe", "/c", "\"C:\\Program Files\\MariaDB 10.5\\bin\\mysqldump.exe\" --quick --lock-tables --user=\"root\" --password=\"2020\" zakupy > \"C:\\Users\\Vadym\\Documents\\testB\\file.sql\" "};
             Process process = null;
             try {
                 process = Runtime.getRuntime().exec(command);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            while (true) {
-                try {
-                    if (!((input.readLine()) != null)) break;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                //String line = input.readLine();
-                //words.add(line);
-                //System.out.println(input.readLine()); //there you can write file
-            }
-            //input.close();
-            try {
-                FileWriter myWriter = new FileWriter("C:\\Users\\Vadym\\Documents\\testB\\filename.txt");
-                myWriter.write(String.valueOf(words));
-                myWriter.close();
                 System.out.println("Successfully wrote to the file.");
             } catch (IOException e) {
-                System.out.println("An error occurred.");
                 e.printStackTrace();
             }
         }
